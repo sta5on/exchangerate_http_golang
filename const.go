5197854b5000643c029144040d6fdb151a9220e2
sta5on <stan.voronet@gmail.com>
2025-07-29 14:09:11 +0300

    added few files

---
+++ const.go
@@ -0,0 +1,5 @@
+package main
+
+const (
+	api_key = "3e6087d3800124a5a3cb77e7"
+)
---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+// https://v6.exchangerate-api.com/v6/3e6087d3800124a5a3cb77e7/latest/USD
+func GetExchangeURL(currency string) string {
+	return fmt.Sprintf("https://v6.exchangerate-api.com/v6/%s/latest/%s", api_key, currency)
+}
+
+func main() {
+	resp, err := http.Get(GetExchangeURL("USD"))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal("Ошибка чтения ответа:", err)
+	}
+
+	fmt.Println("Сырой JSON:")
+	fmt.Println(string(body))
+	fmt.Println("---")
+
+}
