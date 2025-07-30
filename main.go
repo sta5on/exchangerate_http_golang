7cf2d55a1f6ff9d1956f0c4c341a57a8db7c72f2
sta5on <stan.voronet@gmail.com>
2025-07-30 23:16:58 +0300

    mega update, working calculator

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 )
 
 const (
@@ -28,11 +29,52 @@ func main() {
 	//
 
 	curr1 := "USD"
-	curr2 := "EUR"
+	curr2 := "MDL"
+	result := 0.0
 
 	rate1, rate2 := getValues("response.json", curr1, curr2)
-	fmt.Printf("\nFirst Rate %s, curr: %v, second Rate %f, curr %s \n", rate1, curr1, rate2, curr2)
+	if rate1 == 1 || rate1 == 1.0 {
+		fmt.Printf("\nFirst Rate %d, curr: %v, second Rate %.4f, curr %s \n", int(rate1), curr1, rate2, curr2)
+	} else if rate2 == 1 || rate2 == 1.0 {
+		fmt.Printf("\nFirst Rate %.4f, curr: %v, second Rate %d, curr %s \n", rate1, curr1, int(rate2), curr2)
+	} else {
+		fmt.Printf("\nFirst Rate %.4f curr: %v, second Rate %.4f, curr %s \n", rate1, curr1, rate2, curr2)
+	}
+	var amountstr string
+	fmt.Print("Enter amount: ")
 
+	_, err := fmt.Scanln(&amountstr)
+	if err != nil {
+		log.Println("Ошибка ввода:", err)
+	}
+	amount, err := strconv.ParseFloat(amountstr, 64)
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Printf("Вы ввели: %f\n", amount)
+
+	rate1, rate2, amount, result, curr1, curr2 = exchange(rate1, rate2, amount, curr1, curr2)
+	fmt.Println(result)
+	fmt.Printf("\nYou exchange %.f %s TO %s\n%s/%s rate is %f\nFor %f %s YOU WILL GET %f %s\nTHANK YOU!\n", amount, curr1, curr2, curr1, curr2, rate2, amount, curr1, result, curr2)
+	// сделать по разному отображение дробной части
+	// если ее нет то сократить нули если есть то показывать 2 символа наверное
+	//
+}
+
+func exchange(rate1, rate2, amount float64, curr1, curr2 string) (float64, float64, float64, float64, string, string) {
+	//100 юсд в евро
+	//100*0.86=86 EUR
+	//100 евро в mdl
+	//100/0.8681*17.02 = 1957
+
+	result := 0.0
+	if rate1 == 1.0 {
+		fmt.Println("One of you currency is USD")
+		result = amount * rate2
+	}
+	// сделать перевод из чего то другого кроме долларов и наоборот
+
+	return rate1, rate2, amount, result, curr1, curr2
 }
 
 func getRates() (response string) {
@@ -68,6 +110,8 @@ func getValues(file string, curr1 string, curr2 string) (rate1 float64, rate2 fl
 	}
 	return rate1, rate2
 }
+
+//func calculateReturn()
 
 func write_to_file(response string) {
 	filePath := "response.json"
