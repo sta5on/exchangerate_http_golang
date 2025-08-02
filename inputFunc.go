a6c6c52392283b35c57c5cd7adf56fab5317a301
sta5on <stan.voronet@gmail.com>
2025-08-02 17:49:12 +0300

    some cool things

---
+++ inputFunc.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+func currInput() {
+	for {
+		fmt.Print("Enter first currency: ")
+		_, err := fmt.Scanln(&curr1)
+		if err != nil {
+			log.Println("Ошибка ввода:", err)
+			continue
+		}
+
+		curr1 = strings.ToUpper(curr1)
+		if slices.Contains(currencyList[:], curr1) {
+			break
+		}
+		fmt.Printf("%s not found! Try again.\n", curr1)
+	}
+
+	for {
+		fmt.Print("Enter second currency: ")
+		_, err := fmt.Scanln(&curr2)
+		if err != nil {
+			log.Println("Ошибка ввода:", err)
+			continue
+		}
+
+		curr2 = strings.ToUpper(curr2)
+		if slices.Contains(currencyList[:], curr2) {
+			break
+		}
+		fmt.Printf("%s not found! Try again.\n", curr2)
+	}
+
+	fmt.Printf("Вы ввели: %s и %s\n", curr1, curr2)
+}
+
+func amountInput() {
+	for {
+		fmt.Print("Enter amount: ")
+
+		_, err := fmt.Scanln(&amountstr)
+		if err != nil {
+			log.Println("Ошибка ввода:", err)
+			continue
+		}
+		amount, err = strconv.ParseFloat(amountstr, 64)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		if err == nil {
+			break
+		}
+		fmt.Println("Некорректный ввод")
+	}
+	fmt.Printf("Вы ввели: %f\n", amount)
+}
--- main.go
+++ main.go
@@ -3,25 +3,76 @@ package main
 import (
 	"fmt"
 	"log"
-	"slices"
-	"strconv"
-	"strings"
 )
+
+var option int
+var curr1, curr2 string
+var result float64
+var amountstr string
+var amount float64
 
 func main() {
 	log.Println("App is starting")
 	welcomeMsg()
 	defer log.Println("App is ending")
+	// проверка жсон на актуальность
 	go checkFileActual()
+MainLoop:
+	for {
+		for {
+			fmt.Printf("\nChoose your option:\n\n1 - Calculate exchange\n2 - Check rate\n3 - See all currencies\n4 - Exit\n")
+			fmt.Scanln(&option)
+			if option >= 1 && option <= 4 {
+				break
+			}
+			fmt.Println("Invalid operation, please choose 1-4")
+		}
+		switch option {
+		case 1:
+			currInput()
+			rate1, rate2 := getValues("response.json", curr1, curr2)
+			amountInput()
+
+			rate1, rate2, amount, result, curr1, curr2 = exchange(rate1, rate2, amount, curr1, curr2)
+			fmt.Println(result)
+			fmt.Printf("\nYou exchange %.f %s TO %s\n%s/%s rate is %f\nFor %f %s YOU WILL GET %f %s\nTHANK YOU!\n", amount, curr1, curr2, curr1, curr2, rate2, amount, curr1, result, curr2)
+
+			fmt.Printf("Rate is actual for: %d\n", getTime("response.json"))
+
+			hr, mins := getActualityTime()
+			fmt.Printf("Differnce is %d hours and %d minutes\n", hr, mins)
+			fmt.Println(convertTime(getTime("response.json")))
+			fmt.Println("Current time:", getCurrTime())
+		case 2:
+		//
+		case 3:
+			showCurr()
+		case 4:
+			break MainLoop
+		}
+	}
+
+	// если НЕ ДОЛЛАР ТО EUR/MDL rate is 17.024895 строка неверна! исправить!
+	//
+	//+ сделать отдельными функциями getTime и вывод норм сделать
+	//+ в вывод этого говна доабвить дату время актуальности курса! unix тайм взять как то перевести мб или +3 к гмт добавить к тому че имеем хз
+	//
+	// если ее нет то сократить нули если есть то показывать 2 символа наверное
+	// сделать по разному отображение дробной части
 	//
 	//Запрос по апи, сохранение ответа в жсон response.json
 	//
 	//writeToFile(getRates())
 	//
-
+	// ВВОД СУММЫ
+	//}
+	//	fmt.Printf("\nFirst Rate %.4f curr: %v, second Rate %.4f, curr %s \n", rate1, curr1, rate2, curr2)
+	//} else {
+	//	fmt.Printf("\nFirst Rate %.4f, curr: %v, second Rate %d, curr %s \n", rate1, curr1, int(rate2), curr2)
+	//} else if rate2 == 1 || rate2 == 1.0 {
+	//	fmt.Printf("\nFirst Rate %d, curr: %v, second Rate %.4f, curr %s \n", int(rate1), curr1, rate2, curr2)
 	// сделать норм начало проги, ввод валюты, и что бы она сама не закрывалась, только по команде
 	// и всёёёёё
-
 	// ввод валют
 	//
 	// ввод
@@ -31,92 +82,22 @@ func main() {
 	// ввод 2
 	// if contain -> break
 	// if not continue
-	//
-	var curr1, curr2 string
-	for {
-		fmt.Print("Enter first currency: ")
-		_, err := fmt.Scanln(&curr1)
-		if err != nil {
-			log.Println("Ошибка ввода:", err)
-			continue
-		}
-
-		curr1 = strings.ToUpper(curr1)
-		if slices.Contains(currencyList[:], curr1) {
-			break
-		}
-		fmt.Printf("%s not found! Try again.\n", curr1)
-	}
-
-	for {
-		fmt.Print("Enter second currency: ")
-		_, err := fmt.Scanln(&curr2)
-		if err != nil {
-			log.Println("Ошибка ввода:", err)
-			continue
-		}
-
-		curr2 = strings.ToUpper(curr2)
-		if slices.Contains(currencyList[:], curr2) {
-			break
-		}
-		fmt.Printf("%s not found! Try again.\n", curr2)
-	}
-
-	fmt.Printf("Вы ввели: %s и %s\n", curr1, curr2)
-
-	result := 0.0
 	// ВЫБОР ВАЛЮТЫ
-	rate1, rate2 := getValues("response.json", curr1, curr2)
-	if rate1 == 1 || rate1 == 1.0 {
-		fmt.Printf("\nFirst Rate %d, curr: %v, second Rate %.4f, curr %s \n", int(rate1), curr1, rate2, curr2)
-	} else if rate2 == 1 || rate2 == 1.0 {
-		fmt.Printf("\nFirst Rate %.4f, curr: %v, second Rate %d, curr %s \n", rate1, curr1, int(rate2), curr2)
-	} else {
-		fmt.Printf("\nFirst Rate %.4f curr: %v, second Rate %.4f, curr %s \n", rate1, curr1, rate2, curr2)
-	}
-
-	// ВВОД СУММЫ
-	var amountstr string
-	var amount float64
-	for {
-		fmt.Print("Enter amount: ")
-
-		_, err := fmt.Scanln(&amountstr)
-		if err != nil {
-			log.Println("Ошибка ввода:", err)
-			continue
-		}
-		amount, err = strconv.ParseFloat(amountstr, 64)
-		if err != nil {
-			log.Println(err)
-			continue
-		}
-		if err == nil {
-			break
-		}
-		fmt.Println("Некорректный ввод")
-	}
-	fmt.Printf("Вы ввели: %f\n", amount)
-
-	rate1, rate2, amount, result, curr1, curr2 = exchange(rate1, rate2, amount, curr1, curr2)
-	fmt.Println(result)
-	fmt.Printf("\nYou exchange %.f %s TO %s\n%s/%s rate is %f\nFor %f %s YOU WILL GET %f %s\nTHANK YOU!\n", amount, curr1, curr2, curr1, curr2, rate2, amount, curr1, result, curr2)
-	// сделать по разному отображение дробной части
-	// если ее нет то сократить нули если есть то показывать 2 символа наверное
+	//if rate1 == 1 || rate1 == 1.0 {
+	//currInput()
+	//rate1, rate2 := getValues("response.json", curr1, curr2)
+	//amountInput()
 	//
-	//+ в вывод этого говна доабвить дату время актуальности курса! unix тайм взять как то перевести мб или +3 к гмт добавить к тому че имеем хз
-	//+ сделать отдельными функциями getTime и вывод норм сделать
+	//rate1, rate2, amount, result, curr1, curr2 = exchange(rate1, rate2, amount, curr1, curr2)
+	//fmt.Println(result)
+	//fmt.Printf("\nYou exchange %.f %s TO %s\n%s/%s rate is %f\nFor %f %s YOU WILL GET %f %s\nTHANK YOU!\n", amount, curr1, curr2, curr1, curr2, rate2, amount, curr1, result, curr2)
 	//
-
-	// если НЕ ДОЛЛАР ТО EUR/MDL rate is 17.024895 строка неверна! исправить!
-
-	fmt.Printf("Rate is actual for: %d\n", getTime("response.json"))
-
-	hr, mins := getActualityTime()
-	fmt.Printf("Differnce is %d hours and %d minutes\n", hr, mins)
-	fmt.Println(convertTime(getTime("response.json")))
-	fmt.Println("Current time:", getCurrTime())
+	//fmt.Printf("Rate is actual for: %d\n", getTime("response.json"))
+	//
+	//hr, mins := getActualityTime()
+	//fmt.Printf("Differnce is %d hours and %d minutes\n", hr, mins)
+	//fmt.Println(convertTime(getTime("response.json")))
+	//fmt.Println("Current time:", getCurrTime())
 
 	fmt.Printf("\n\n0_0\n\n")
 }
